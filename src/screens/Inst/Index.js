a273997d87bde26110a09072109c9f8613d25092
Rafael Alves Freitas da Silva <46302153+rafaelafsinfo@users.noreply.github.com>
2024-06-11 12:06:26 -0300

    update inst

--- src/screens/Inst/Index.js
+++ src/screens/Inst/Index.js
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,FlatList } from 'react-native'
 import React from 'react'
 import { useState,useEffect } from 'react';
 import api from '../../services/api';
+import Card from '../../components/Card'
+
 import {useRoute} from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Inst() {
     const route = useRoute();
@@ -16,9 +19,27 @@ export default function Inst() {
     })
   })
   return (
-    <View>
-      <Text>{data}</Text>
-    </View>
+    <SafeAreaView>
+      <Text>{JSON.stringify(data)}</Text>
+
+      <FlatList style={styles.cards}
+        data={data}
+        renderItem={({item}) => <Card
+        cnpj={item.cnpj}
+        nome_inst={item.nome_inst}
+        rua={item.rua}
+        numero={item.numero}
+        bairro={item.bairro}
+        cidade={item.cidade}
+        estado={item.estado}
+        cep={item.CEP}
+        descricao={item.descricao}
+        />}
+        keyExtractor={item => item.cnpj}
+        />
+
+
+    </SafeAreaView>
   )
 }
 
