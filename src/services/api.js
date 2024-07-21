bf87574e3219eee1f2c8f16e2f73cbff25459ac8
Rafael Alves Freitas da Silva <46302153+rafaelafsinfo@users.noreply.github.com>
2024-07-21 10:43:28 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://15.228.203.122:3000',
+    baseURL:'http://15.228.191.54:3000',
     headers:{
         'Access-Control-Allow-Origin': '*'
     }
