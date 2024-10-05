cae7c8d4dda5236a0fa03d8521d41660313dcd0e
Leonardo Faria <lrsf9991@gmail.com>
2024-10-05 10:02:27 -0300

    update

--- src/screens/Usuario/InstPage/index.jsx
+++ src/screens/Usuario/InstPage/index.jsx
@@ -5,7 +5,7 @@ import * as Animatable from 'react-native-animatable'
 import {useRoute} from '@react-navigation/native'
 import api from '../../../services/api'
 import { UserContext } from '../../../contexts/UserContext';
-import Icon from 'react-native-vector-icons/Feather';
+import { Feather } from "@expo/vector-icons";
 import { useNavigation } from '@react-navigation/native'
 
 export default function InstPage() {
@@ -13,13 +13,22 @@ export default function InstPage() {
   const { cnpj,nome_inst,rua,numero,bairro,cidade,estado,cep,descricao } = route.params
   const navigation = useNavigation()
   const { user } = useContext(UserContext)
-  const[btn1Visible,setbtn1Visible] = useState(true)
-  const[btn2Visible,setbtn2Visible] = useState(false)
   const [idUsuario,setIdUsuario] = useState("")
   const [idInstituicao,setIdinstituicao] = useState("")
   const [produto,setProduto] = useState('')
   const [data,setData] = useState('')
   const [error,setError] = useState(null)
+  const[btn1Visible,setbtn1Visible] = useState(true)
+  const[btn2Visible,setbtn2Visible] = useState(false)
+  const [isBackgroundDark, setIsBackgroundDark] = useState(false)
+
+  const today = new Date()
+  const formattedDate = formatDate(today,'mm-dd-aaaa')
+  useEffect(() => {
+    setData(formattedDate)
+    setIdUsuario(user.id)
+    setIdinstituicao(cnpj)
+  },[])
 
   function formatDate(date,format){
     const map = {
@@ -30,18 +39,10 @@ export default function InstPage() {
       return format.replace(/mm|dd|aaaa/gi, matched => map[matched])
   }
 
-  const today = new Date()
-  const formattedDate = formatDate(today,'mm-dd-aaaa')
-  console.log(formattedDate)
-  useEffect(() => {
-    setData(formattedDate)
-    setIdUsuario(user.id)
-    setIdinstituicao(cnpj)
-  },[])
-
   const handlepress = () => {
     setbtn1Visible(!btn1Visible)
     setbtn2Visible(!btn2Visible)
+    setIsBackgroundDark(true);
   }
 
   const handleSubmit = async () =>{
@@ -66,20 +67,19 @@ export default function InstPage() {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
+   <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Animatable.View animation='fadeInLeft' delay={500} style={styles.containerHeader}>
+            <Feather
+                style={styles.BackButton}
+                name="arrow-left"
+                size={25}
+                onPress={() => navigation.navigate("MainPage")}
+            />
+            <Text style={styles.InstitutionName}>{nome_inst}</Text>
+        </Animatable.View>
 
-      <Animatable.View animation='fadeInLeft' delay={500} style={styles.containerHeader}>
-        <TouchableOpacity
-            style={styles.botaovoltar}
-            onPress={()=>navigation.navigate('MainPage')}>
-          <Icon name="arrow-left" size={20} color="#fff" />
-        </TouchableOpacity>
-        <Text style={styles.message}>{nome_inst}</Text>
-      </Animatable.View>
-
-      <Animatable.View style={styles.containerForm}>
-        <Animatable.View animation='fadeInUp' delay={600}>
+        <Animatable.View animation='fadeInUp' delay={600} style={styles.containerForm}>
           <Animatable.Text animation='slideInLeft' delay={900}>
             <Text style={styles.detailstext}>Cnpj:{'\n'}</Text>
             {cnpj}
@@ -91,7 +91,7 @@ export default function InstPage() {
           <Animatable.Text animation='slideInLeft' delay={900}>
             <Text style={styles.detailstext}>Bairro:{'\n'}</Text>
             {bairro}
-            </Animatable.Text>
+          </Animatable.Text>
           <Animatable.Text animation='slideInRight' delay={900}>
             <Text style={styles.detailstext}>Cidade:{'\n'}</Text>
             {cidade} - {estado}
@@ -104,107 +104,92 @@ export default function InstPage() {
             <Text style={styles.detailstext}>Descrição:{'\n'}</Text>
             {descricao}
           </Animatable.Text>
-        </Animatable.View>
-        
-        <Animatable.View animation='fadeInUp' delay={1200} style={styles.root}>
-          <View style={styles.content}>
-
-            
-          {btn1Visible && ( <TouchableOpacity 
-          style={styles.button}
-          onPress={handlepress}>
-            <Text style={styles.buttonText}>doar</Text>
-          </TouchableOpacity>)}
-
-          {btn2Visible && (
-            <SafeAreaView>
-            
-              <TextInput
-                value={produto}
-                style={styles.input}
-                placeholder='Digite o produto a ser doado'
-                keyboardType='default'
-                
-                onChangeText={text => setProduto(text)}
-                />
-                
-              <TouchableOpacity 
+          {btn1Visible && (
+            <TouchableOpacity 
             style={styles.button}
-            onPress={handleSubmit}>
+            onPress={handlepress}>
               <Text style={styles.buttonText}>doar</Text>
             </TouchableOpacity>
-            
-              <TouchableOpacity 
+          )}
+          {btn2Visible && (
+          <View style={styles.containerDonation}>
+            <TextInput
+              value={produto}
+              style={styles.input}
+              placeholder='Digite o produto a ser doado'
+              keyboardType='default'
+              onChangeText={text => setProduto(text)}
+            />
+
+            <TouchableOpacity 
+            style={styles.button}
+            onPress={handleSubmit}>
+                <Text style={styles.buttonText}>doar</Text>
+            </TouchableOpacity>
+              
+            <TouchableOpacity 
             style={styles.button}
             onPress={handlepress}>
               <Text style={styles.buttonText}>cancelar</Text>
             </TouchableOpacity>
-          </SafeAreaView>
-        )}
-
           </View>
+        )}
         </Animatable.View>
-        
-      </Animatable.View>
-
       </ScrollView>
+      {isBackgroundDark && (
+        <View style={styles.darkOverlay} />
+      )}
     </SafeAreaView>
-  )
-}
+)}
 
 const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:'#f9f7f8',
   },
-  containerHeader:{
-    width: '100%',
-    height: '20%',
-    borderBottomLeftRadius:25,
-    borderBottomRightRadius:25,
-    alignItems: 'center',
-    paddingTop: '25%',
-    backgroundColor:'#4e0189' 
+  darkOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    zIndex: 10,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
   },
   containerForm:{
-    flex:1,
+    flexGrow: 1,
     backgroundColor: '#f6f7f9',
-    marginTop:'10%',
+    paddingTop:'10%',
     paddingStart:'7%',
     paddingEnd:'7%',
+    paddingBottom: '100%',
   },
-  message:{
-    fontSize:28,
-    fontWeight:'bold',
-    color:'#fff',
+  detailstext:{
+    fontSize:18,
+    fontWeight:'bold'
   },
-  title:{
-    fontSize:20,
-    marginTop:28,
-    color:'#ff8c00'
-  },
-  qr: {
-    padding: 15,
-  },
-  root: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  content: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: 'center',
+  //---------------------------------------
+  containerDonation:{
+    position:'absolute',
+    backgroundColor:'#4e0189',
+    borderRadius:20,
+    alignSelf:'center',
+    alignItems:'center',
+    padding:23,
+    borderColor:'black'
   },
   button:{
     position:'relative',
     backgroundColor:'#ff8c00',
     borderRadius:50,
-    paddingVertical:8,
-    width:'60%',
+    paddingVertical:10,
+    marginTop:20,
+    width:'50%',
     alignSelf:'center',
-    marginBottom:'2.5%',
     alignItems:'center',
     justifyContent:'center'
   },
@@ -219,21 +204,34 @@ const styles = StyleSheet.create({
     borderRadius:10,
     paddingStart:8,
     paddingEnd:8,
+    marginTop:20,
     height:40,
     marginBottom:12,
     fontSize:16,
-    borderColor:'#cdd1e0'
+    borderColor:'#cdd1e0',
+    backgroundColor: '#f9f7f8'
   },
-  botaovoltar:{
-    position: 'absolute',
-    top: 10,
-    left:10,
-    padding: 10,
-    zIndex: 1000  
+  
+  
+  //--------------------------------------
+  containerHeader:{
+    width:'100%',
+    height:120,
+    borderBottomLeftRadius:25,
+    borderBottomRightRadius:25,
+    backgroundColor:'#4e0189',
+    flexDirection:'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 10,
   },
-  detailstext:{
-    fontSize:18,
-    fontWeight:'bold'
+  BackButton:{
+    color: "#fff",
+  },
+  InstitutionName:{
+    left:'50%',
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#fff",
   }
-
 })
